ab04ab76ee296f6ab793fc9a26ba02bc4a317c00
saumyasanu <saumya.sanu6448@gmail.com>
2026-02-11 10:49:01 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ 
+  int x,y,z;
+  printf("i am in section g2j\n");
+  printf("enter two numbers- %d%d \n", x,y);
+  scanf("%d%d",&x,&y);
+  z=x+y;
+  printf("sum of numbers is-%d",z);
+  return 0;
+}
